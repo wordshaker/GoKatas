3510d2eacae9c2220c78cf8e3bef853fb805ac3f
Jessica White <code.jessw@gmail.com>
2017-05-01 17:10:34 +0100

    First Refactor

--- src/fizzbuzz/fizzbuzz.go
+++ src/fizzbuzz/fizzbuzz.go
@@ -7,15 +7,29 @@ func FizzBuzz(num int) string{
 	var name = ""
 
 	for i :=1; i < num+1; i ++ {
-		if i%3 == 0 && i%5 == 0 {
+		if ModOfThree(i) && ModOfFive(i){
 			name += "FizzBuzz"
-		} else if i%3 == 0 {
+		} else if ModOfThree(i) {
 			name += "Fizz"
-		} else if i%5 == 0 {
+		} else if ModOfFive(i) {
 			name += "Buzz"
 		} else {
 			name += strconv.Itoa(i)
 		}
 	}
 	return name
+}
+
+func ModOfThree(x int) bool{
+	if x%3 == 0{
+		return true
+	}
+	return false
+}
+
+func ModOfFive(x int) bool{
+	if x%5 == 0{
+		return true
+	}
+	return false
 }
