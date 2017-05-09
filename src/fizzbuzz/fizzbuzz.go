33fdb0007194aace0b1ffe9f4ac51bfbf417a9ab
Jessica White <code.jessw@gmail.com>
2017-05-09 21:02:00 +0100

    simplify fizzbuzz

--- src/fizzbuzz/fizzbuzz.go
+++ src/fizzbuzz/fizzbuzz.go
@@ -7,29 +7,15 @@ func FizzBuzz(num int) string{
 	var name = ""
 
 	for i :=1; i < num+1; i ++ {
-		if ModOfThree(i) && ModOfFive(i){
+		if i%3 == 0 && i%5 == 0{
 			name += "FizzBuzz"
-		} else if ModOfThree(i) {
+		} else if i%3 == 0 {
 			name += "Fizz"
-		} else if ModOfFive(i) {
+		} else if i%5 == 0 {
 			name += "Buzz"
 		} else {
 			name += strconv.Itoa(i)
 		}
 	}
 	return name
-}
-
-func ModOfThree(x int) bool{
-	if x%3 == 0{
-		return true
-	}
-	return false
-}
-
-func ModOfFive(x int) bool{
-	if x%5 == 0{
-		return true
-	}
-	return false
 }
